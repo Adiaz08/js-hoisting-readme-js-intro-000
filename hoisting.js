971e381dbe4337a9c2182fe7994d14a235e6d312
Anndy Diaz <anndydiaz@live.com>
2018-08-06 08:17:44 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -5,24 +5,26 @@
   console.log("so call me");
   var lyric = "maybe";
   return lyric;
+
 }
 
 
 function crazy() {
  // fix the code in here:
-
-  var thisIsCrazy = function (){
+var thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
 }
 
-function sayMyName() {
-function sayMyName(){// fix the code in here:
-  saymy();
-function sayMy() {
 
-    var name = "Kristin"
+function sayMyName() {
+// fix the code in here:
+  var name = "Cricky";
+
+function sayMy() {
+  var name = "Kristin"
     console.log(name);
-  }
+  };
+  sayMy()
 }
